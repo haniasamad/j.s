05b238e374f78ab2f30997d7b717d6389a0fc271
Hania Samad <141482243+haniasamad@users.noreply.github.com>
2024-06-25 22:37:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var num1 = + (prompt("Enter your any first number"))
+var num2 = +(prompt("Enter your any second number"))
+var num3 = +(prompt("Enter your any thyird number"))
+var num4=(num1+num2+num3)
+alert(num4)
+
+var num4=(num1-num2-num3)
+alert(num4)
+
+
+var num4=(num1*num2*num3)
+alert(num4)
+
+
+var num4=(num1%num2%num3)
+alert(num4)
